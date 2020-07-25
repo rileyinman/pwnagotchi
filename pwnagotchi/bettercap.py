969adcd5a8406f96ff10d0762f9332028d6cfab9
Riley Inman <rileyminman@gmail.com>
2020-07-25 19:13:15 -0400

    bettercap: Add missing f-strings

--- pwnagotchi/bettercap.py
+++ pwnagotchi/bettercap.py
@@ -12,7 +12,7 @@ def decode(r, verbose_errors=True):
         if r.status_code == 200:
             logging.error(f"Error while decoding json: error='{e}' resp='{r.text}'")
         else:
-            err = f"error {r.status_code}: {r.text.strip()}"
+            err = f"Error {r.status_code}: {r.text.strip()}"
             if verbose_errors:
                 logging.info(err)
             raise Exception(err)
@@ -26,8 +26,8 @@ class Client(object):
         self.port = port
         self.username = username
         self.password = password
-        self.url = "{scheme}://{hostname}:{port}/api"
-        self.websocket = "ws://{username}:{password}@{hostname}:{port}/api"
+        self.url = f"{scheme}://{hostname}:{port}/api"
+        self.websocket = f"ws://{username}:{password}@{hostname}:{port}/api"
         self.auth = HTTPBasicAuth(username, password)
 
     def session(self):
@@ -35,7 +35,7 @@ class Client(object):
         return decode(r)
 
     async def start_websocket(self, consumer):
-        s = "{self.websocket}/events"
+        s = f"{self.websocket}/events"
         while True:
             try:
                 async with websockets.connect(s, ping_interval=60, ping_timeout=90) as ws:
